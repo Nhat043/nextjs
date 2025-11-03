7f1c6a53e142862426b54d3a1a39f8d8e0f9e137
Meo0432004 <meocho0432004@gmail.com>
2025-11-03 08:07:25 +0700

    CRUD invoices

--- chap1/nextjs-dashboard/next.config.ts
+++ chap1/nextjs-dashboard/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   cacheComponents: true  // PPR is now enabled via cacheComponents in Next.js 16 canary
+  // }
 };
 
 export default nextConfig;
